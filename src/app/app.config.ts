168337979d3a6fbec74fed3fc5f44e820bbacda3
Simone Chan <zwsimone@gmail.com>
2025-10-08 23:19:06 +0200

    Import PrimeNG and styling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,11 +3,19 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { providePrimeNG } from 'primeng/config';
+
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
   ]
 };
