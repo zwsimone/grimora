009dd910ac6812e83d5b0db2b6e6a4b53913f440
Simone Chan <zwsimone@gmail.com>
2025-10-14 23:03:17 +0200

    Add more editor toolbar items

--- src/app/shared/components/grid/grid-text.component.ts
+++ src/app/shared/components/grid/grid-text.component.ts
@@ -6,7 +6,18 @@ import { TiptapEditorDirective } from 'ngx-tiptap';
 import { Editor } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
 import Placeholder from '@tiptap/extension-placeholder';
-import { Bold, Heading1, Italic, LucideAngularModule, Pilcrow } from 'lucide-angular';
+import TextAlign from '@tiptap/extension-text-align';
+import Code from '@tiptap/extension-code';
+import Strike from '@tiptap/extension-strike';
+import Underline from '@tiptap/extension-underline';
+import Highlight from '@tiptap/extension-highlight';
+import Link from '@tiptap/extension-link';
+
+// PrimeNG modules
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { ButtonModule } from 'primeng/button';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { StyleClassModule } from 'primeng/styleclass';
 
 // some custom components
 @Component({
@@ -18,23 +29,42 @@ import { Bold, Heading1, Italic, LucideAngularModule, Pilcrow } from 'lucide-ang
     FormsModule,
     ReactiveFormsModule,
     TiptapEditorDirective,
-    LucideAngularModule,
+    SelectButtonModule,
+    ButtonModule,
+    ToggleButtonModule,
+    StyleClassModule,
   ],
 })
 export class GridTextComponent extends BaseWidget implements OnDestroy {
   @Input() value: string = '<p>Hello, Tiptap!</p>'; // test custom input data
 
-  readonly Heading1 = Heading1;
-  readonly Bold = Bold;
-  readonly Italic = Italic;
-  readonly Pilcrow = Pilcrow;
+  justifyOptions = [
+    { justify: 'left', icon: 'bi bi-text-left' },
+    { justify: 'center', icon: 'bi bi-text-center' },
+    { justify: 'right', icon: 'bi bi-text-right' },
+    { justify: 'justify', icon: 'bi bi-justify' },
+  ];
 
   reactiveForm = new FormGroup({
     content: new FormControl(this.value),
   });
 
   editor = new Editor({
-    extensions: [StarterKit, Placeholder],
+    extensions: [
+      StarterKit,
+      Placeholder,
+      TextAlign.configure({ types: ['heading', 'paragraph'] }),
+      Code,
+      Strike,
+      Underline,
+      Highlight,
+      Link.configure({
+        openOnClick: true,
+        autolink: true,
+        defaultProtocol: 'https',
+        protocols: ['http', 'https', 'mailto'],
+      }),
+    ],
     editorProps: {
       attributes: {
         class: 'grid-text-editor',
